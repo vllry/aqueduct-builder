747d948d38cc7381623d84645bb645e7aa41abff
vllry <vallery@messages.xyz>
2015-07-17 13:29:10 -0700

    (Untested commit) refactored libaqueductbuilder

--- libaqueductbuilder.py
+++ libaqueductbuilder.py
@@ -3,11 +3,14 @@
 import json
 import tarfile
 import sqlite3
-from os import path, popen, remove, chdir
+from os import path, popen, remove, chdir, mkdir
 
 
 
 conf = json.load(open('aqueduct-builder.conf'))
+for d in conf['dir']: #Ensure all dirs have a trailing slash
+	if conf['dir'][d][-1] != '/':
+		conf['dir'][d] = conf['dir'][d] + '/'
 
 
 
@@ -21,25 +24,31 @@ def db_new_build_id(os, release, submitted_file):
 
 
 
-def pbuilder_debuild(filepath, release):
+def pbuilder_debuild(buildid, filepath, release):
+	tgz = conf['path']['basetgz'] % (release)
+	dir_result = conf['dir']['result'] % str(buildid)
+	mkdir(result)
 	chdir(filepath)
-	return popen('pdebuild -- --basetgz /var/cache/pbuilder/%s.base.tgz' % (release)).read()
+	popen('pdebuild -- --basetgz %s --result %s > %sbuild.log' % (tgz, dir_result, dir_result))
+
 
 
 def pbuilder_basetgz_exists(release):
-	return path.exists('/var/cache/pbuilder/' + release + '.base.tgz')
+	tgz = conf['path']['basetgz'] % (release)
+	return path.exists(tgz)
+
 
 
 def pbuilder_basetgz_create(release):
-	print(popen('pbuilder --create --distribution ' + release + ' --basetgz /var/cache/pbuilder/' + release + '.base.tgz').read())
+	tgz = conf['path']['basetgz'] % (release)
+	print(popen('pbuilder --create --distribution %s --basetgz %s' % (release, tgz)).read())
+
 
 
 def pbuilder_basetgz_update(release):
-	print(popen('pbuilder --update --distribution ' + release + ' --basetgz /var/cache/pbuilder/' + release + '.base.tgz').read())
+	tgz = conf['path']['basetgz'] % (release)
+	print(popen('pbuilder --update --distribution %s --basetgz %s' % (release, tgz)).read())
 
-
-def pbuilder_build(release):
-	print(popen('pbuilder --debuild --distribution ' + release + ' --basetgz /var/cache/pbuilder/' + release + '.base.tgz').read())
 
 
 def untar(filepath, dest):
@@ -54,7 +63,7 @@ def untar(filepath, dest):
 
 def pkg_build(os, release, filepath):
 	filepath = untar(filepath, conf['dir']['processing'])
-	filepath = conf['dir']['processing'] + '/' + filepath
+	filepath = conf['dir']['processing'] + filepath
 
 	if os in ('debian', 'ubuntu'):
 		if not pbuilder_basetgz_exists(release):
@@ -62,10 +71,7 @@ def pkg_build(os, release, filepath):
 		#else:
 			#pbuilder_basetgz_update(release)
 
-		buildlog = pbuilder_debuild(filepath, release)
-		print("before")
-		print(buildlog)
-		print("after")
+		pbuilder_debuild(filepath, release)
 
 	else:
 		print('Unsupported os: ' + os)
