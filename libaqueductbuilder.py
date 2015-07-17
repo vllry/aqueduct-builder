608589dc1ab6d8421d9a52f9eba65483d81216d7
Vallery Lancey <vallery@messages.xyz>
2015-07-17 08:08:16 -0700

    Got basic deb building working

--- libaqueductbuilder.py
+++ libaqueductbuilder.py
@@ -3,7 +3,7 @@
 import json
 import tarfile
 import sqlite3
-from os import path, popen
+from os import path, popen, remove, chdir
 
 
 
@@ -17,7 +17,13 @@ def db_new_build_id(os, release, submitted_file):
 	cur.execute("INSERT INTO builds (os, release, submitted_file) VALUES('%s', '%s', '%s')" % (os,release,submitted_file))
 	con.commit()
 	cur.execute("SELECT last_insert_rowid()")
-	return cur.fetchone()
+	return cur.fetchone()[0]
+
+
+
+def pbuilder_debuild(filepath, release):
+	chdir(filepath)
+	return popen('pdebuild -- --basetgz /var/cache/pbuilder/%s.base.tgz' % (release)).read()
 
 
 def pbuilder_basetgz_exists(release):
@@ -36,20 +42,30 @@ def pbuilder_build(release):
 	print(popen('pbuilder --debuild --distribution ' + release + ' --basetgz /var/cache/pbuilder/' + release + '.base.tgz').read())
 
 
-def untar(path, dest):
-	tfile = tarfile.open(path, 'r:gz')
+def untar(filepath, dest):
+	tfile = tarfile.open(filepath, 'r:gz')
 	tfile.extractall(dest)
+	name = tfile.getnames()[0]
+	tfile.close() #?
+	remove(filepath)
+	return name
 
 
-def pkg_build(os, release, path):
-	untar(path, conf['dir']['processing'])
-	filename = path.split('/')[-1]
 
-	if os == "debian":
+def pkg_build(os, release, filepath):
+	filepath = untar(filepath, conf['dir']['processing'])
+	filepath = conf['dir']['processing'] + '/' + filepath
+
+	if os in ('debian', 'ubuntu'):
 		if not pbuilder_basetgz_exists(release):
 			pbuilder_basetgz_create(release)
-		else:
-			pbuilder_basetgz_update(release)
+		#else:
+			#pbuilder_basetgz_update(release)
+
+		buildlog = pbuilder_debuild(filepath, release)
+		print("before")
+		print(buildlog)
+		print("after")
 
 	else:
 		print('Unsupported os: ' + os)
